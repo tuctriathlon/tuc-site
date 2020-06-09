3666a4bb162ebfbfe7fe93a0962e13d9ba57f61c
Francis Tosolini <francis.tosolini@gmail.com>
2020-06-09 18:20:54 +0200

    fix: faq search

--- src/faq/faq-search.pipe.ts
+++ src/faq/faq-search.pipe.ts
@@ -11,7 +11,8 @@ export class FaqSearchPipe implements PipeTransform {
     const words = unidecode(searchText)
       .split(/\s|\W/)
       .filter(w => w.length > 2);
-    return (value || []).filter(v => !words.length || words.some(w => v.reponse.includes(w) || v.question.includes(w)));
+    return (value || []).filter(v => !words.length
+      || words.some(w => unidecode(v.reponse).includes(w) || unidecode(v.question).includes(w)));
   }
 
 }
