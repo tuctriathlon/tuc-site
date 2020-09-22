b0624e4c81d04e243d76ff107befb97eb2da6545
Francis Tosolini <francis.tosolini@gmail.com>
2020-09-22 23:37:05 +0200

    fix: affichage gpx

--- src/shared/map/map.component.ts
+++ src/shared/map/map.component.ts
@@ -7,6 +7,11 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
 import * as Proj from 'ol/proj';
+import Style from 'ol/style/Style';
+import {environment} from '../../environments/environment';
+import Stroke from 'ol/style/Stroke';
+import Geometry from 'ol/geom/Geometry';
+import SimpleGeometry from 'ol/geom/SimpleGeometry';
 
 @Component({
   selector: 'app-map',
@@ -17,27 +22,40 @@ export class MapComponent implements AfterViewInit {
   @Input()gpx: string;
 
   private map: Map;
+  private gpxLayer: VectorLayer;
   constructor() {
   }
 
   ngAfterViewInit(): void {
+    const source = new VectorSource({
+      url: environment.production ? `../../files/${this.gpx}`
+        : `${environment.directusUrl}/uploads/${environment.directusProject}/originals/${this.gpx}`,
+      format: new GPX()
+    });
+    this.gpxLayer = new VectorLayer({
+      source
+    });
+    this.gpxLayer.setStyle(new Style({stroke: new Stroke({color: 'firebrick', width: 5})}));
     this.map = new Map({
       target: 'map',
       layers: [
         new TileLayer({
           source: new OSM(),
         }),
-        new VectorLayer({
-          source: new VectorSource({
-            url: this.gpx,
-            format: new GPX()
-          })
-        })
+        this.gpxLayer
       ],
       view: new View({
-        center: Proj.fromLonLat([1.4333, 43.6]),
+        center: Proj.fromLonLat([1.43333, 43.6]),
         zoom: 10
       })
+    });
+    this.gpxLayer.on('change', (e) => {
+      if (source.getState() === 'ready') {
+        const feature = source.getFeatures()[0];
+        const polygon = feature.getGeometry() as SimpleGeometry;
+        // TODO tracer le profile polygon.flatCoordinates[index]%4 === 2 D3js power
+        this.map.getView().fit(polygon, {padding: [50, 50, 50, 50]});
+      }
     });
   }
 
